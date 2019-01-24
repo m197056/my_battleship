2df55111ecaf65b03d1046a090eaf48c8a3cd349
John Donnal <donnal@usna.edu>
2019-01-23 20:48:38 -0500

    lab1_start

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import sys
 import colors
 from game_board import GameBoard
 
-
 BLOCK_SIZE = 30
 NBLOCKS = 11
 TOP_MARGIN = 30
@@ -18,16 +17,40 @@ def main():
                                                       BLOCK_SIZE * NBLOCKS + TOP_MARGIN + PADDING))
     screen.fill(colors.screen_bkgd)
     pygame.display.set_caption('USNA Battleship')
+
+    # size of the game board figure based on BLOCK SIZE pixels
     board_dimension = (BLOCK_SIZE * NBLOCKS, BLOCK_SIZE * NBLOCKS)
 
-    # draw a single game board
+    # "my" game board has my ships
     my_board: GameBoard = GameBoard(board_dimension)
     my_board.rect.top = TOP_MARGIN
     my_board.rect.left = PADDING
-    my_board.refresh()
 
+    # "their" game board has my guesses
+    their_board: GameBoard = GameBoard(board_dimension)
+    # position their_board PADDING pixels to the right of my_board
+    their_board.rect.top = TOP_MARGIN
+    their_board.rect.left = PADDING*2+my_board.rect.width
+
+    # paint the board surface
+    my_board.refresh()
+    their_board.refresh()
+
+    # --------- BEGIN YOUR CODE ----------
+    # add titles above the game boards
+
+    # draw 'YOU' centered above my_board
+
+    # draw 'THEM' centered above their_board
+
+    # --------- END YOUR CODE ------------
+
+    # place the board on the screen
+    their_board.draw(screen)
     my_board.draw(screen)
+
     while True:
+        # wait for user to click 'X' button
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
